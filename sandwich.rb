ebde6597d73939d2ae657316e6b9e2cb71d0ad41
anjalisebastian <anjalisebastian@icloud.com>
2018-07-13 15:32:51 +0000

    automatically backed up by learn

--- sandwich.rb
+++ sandwich.rb
@@ -1,4 +1,8 @@
 class Sandwich
   def initialize(bread_type, meat, cheese_type, hot_or_cold)
+    @bread_type = bread_type
+    @meat = meat
+    @cheese_type = cheese_type
+    @hot_or_cold = hot_or_cold
     
 end
